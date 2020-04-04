960dec25cc025907aaa2fb00b425e2adf97e7c70
andyg <ajgatwork@gmail.com>
2020-04-04 15:28:03 +0100

    add debugging tooltip

--- mb.js
+++ mb.js
@@ -2,27 +2,27 @@
 
 
 const log2 = Math.log(2);
-  
+
 function mbCalc(p, maxIteration, escapeValue) {
-  
-  let x2=0;
-  let y2=0;
-  let w=0;
-  let i=0;
-  let x=0;
-  let y=0;
-  while ( x2+y2<=escapeValue && i<maxIteration) {
-    x=x2-y2+p.x;
-    y=w-x2-y2-p.y;
-    x2=x * x;
-    y2=y*y;
-    w=(x+y) * (x+y);
+
+  let x2 = 0;
+  let y2 = 0;
+  let w = 0;
+  let i = 0;
+  let x = 0;
+  let y = 0;
+  while (x2 + y2 <= escapeValue && i < maxIteration) {
+    x = x2 - y2 + p.x;
+    y = w - x2 - y2 - p.y;
+    x2 = x * x;
+    y2 = y * y;
+    w = (x + y) * (x + y);
     i++;
   }
-  p.iteration=i;
+  p.iteration = i;
   //calculate the smoothed iteration
-  if ( i < maxIteration) {
-    p.smoothedIteration = i+1-Math.log((Math.log(x2+y2)/2)/log2)/log2;
+  if (i < maxIteration) {
+    p.smoothedIteration = i + 1 - Math.log((Math.log(x2 + y2) / 2) / log2) / log2;
   } else {
     p.smoothedIteration = maxIteration;
   }
@@ -31,11 +31,11 @@ function mbCalc(p, maxIteration, escapeValue) {
 }
 
 class Point {
-  constructor(x,y) {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.iteration=undefined;
-    this.smoothedIteration=undefined;
+    this.iteration = undefined;
+    this.smoothedIteration = undefined;
   }
 }
 
@@ -49,24 +49,23 @@ class Colour {
 
   static convertString(string) {
     // string in format #rrggbb
-    return new Colour(parseInt(string.substring(1,3),16),   
-                      parseInt(string.substring(3,5),16),
-                      parseInt(string.substring(5),16));
+    return new Colour(parseInt(string.substring(1, 3), 16),
+      parseInt(string.substring(3, 5), 16),
+      parseInt(string.substring(5), 16));
   }
 }
 
 //global const for black
-const BLACK = new Colour(0,0,0);
+const BLACK = new Colour(0, 0, 0);
 
 class IterationRange {
   constructor(lower, higher) {
-    this.lower=lower;
-    this.higher=higher;
+    this.lower = lower;
+    this.higher = higher;
   }
 }
 
-function initPoints(xlower,xhigher,ylower,yhigher,widthPoints,heightPoints)
-{
+function initPoints(xlower, xhigher, ylower, yhigher, widthPoints, heightPoints) {
   let w = xhigher - xlower;
   let h = yhigher - ylower;
   let winterval = w / widthPoints;
@@ -74,25 +73,25 @@ function initPoints(xlower,xhigher,ylower,yhigher,widthPoints,heightPoints)
 
   let pointArray = [];
 
-  for (var j=yhigher,ycount=0;ycount<heightPoints; j -=hinterval,ycount++) {
-    for (var i=xlower,xcount=0;xcount<widthPoints; i +=winterval, xcount++) {
-      pointArray.push(new Point(i,j));
+  for (var j = yhigher, ycount = 0; ycount < heightPoints; j -= hinterval, ycount++) {
+    for (var i = xlower, xcount = 0; xcount < widthPoints; i += winterval, xcount++) {
+      pointArray.push(new Point(i, j));
     }
   }
   return pointArray;
 }
 
-function calculate(pointArray,maxIteration,f, escapeValue) {
+function calculate(pointArray, maxIteration, f, escapeValue) {
   let lowest = maxIteration;
   let highest = 0;
 
-  for(let i=0;i<pointArray.length;i++) {
-    pointArray[i] = f(pointArray[i], maxIteration,escapeValue);
-    if (pointArray[i].iteration < lowest) lowest= pointArray[i].iteration;
-    if (pointArray[i].iteration > highest) highest= pointArray[i].iteration;
+  for (let i = 0; i < pointArray.length; i++) {
+    pointArray[i] = f(pointArray[i], maxIteration, escapeValue);
+    if (pointArray[i].iteration < lowest) lowest = pointArray[i].iteration;
+    if (pointArray[i].iteration > highest) highest = pointArray[i].iteration;
   }
 
-  return new IterationRange(lowest,highest);
+  return new IterationRange(lowest, highest);
 }
 
 // with iteration range 1 to 3 INCLUSIVE (3 slots)
@@ -103,24 +102,24 @@ function createColourRange(lowColour, highColour, iterationRange, maxIterations)
   let colourMap = new Map();
 
   //number of colours that we need to generate
-  let colourCount = iterationRange.higher-iterationRange.lower;
+  let colourCount = iterationRange.higher - iterationRange.lower;
   //want to make sure that the set is always black
-  if (iterationRange.higher==maxIterations) {
+  if (iterationRange.higher == maxIterations) {
     colourCount--;
-    colourMap.set(maxIterations,BLACK);
-    colourMap.set(iterationRange.higher-1,highColour);   
+    colourMap.set(maxIterations, BLACK);
+    colourMap.set(iterationRange.higher - 1, highColour);
   } else {
-    colourMap.set(iterationRange.higher,highColour);
+    colourMap.set(iterationRange.higher, highColour);
   }
-  let redIncrement=(lowColour.red-highColour.red)/colourCount;
-  let blueIncrement=(lowColour.blue-highColour.blue)/colourCount;
-  let greenIncrement=(lowColour.green-highColour.green)/colourCount;
- 
-  for(let i=0;i<colourCount;i++) {
-    colourMap.set(iterationRange.lower+i,new Colour(Math.round(lowColour.red-(i*redIncrement)),
-                                                    Math.round(lowColour.green-(i*greenIncrement)),                                                
-                                                    Math.round(lowColour.blue-(i*blueIncrement))
-                                                    ));
+  let redIncrement = (lowColour.red - highColour.red) / colourCount;
+  let blueIncrement = (lowColour.blue - highColour.blue) / colourCount;
+  let greenIncrement = (lowColour.green - highColour.green) / colourCount;
+
+  for (let i = 0; i < colourCount; i++) {
+    colourMap.set(iterationRange.lower + i, new Colour(Math.round(lowColour.red - (i * redIncrement)),
+      Math.round(lowColour.green - (i * greenIncrement)),
+      Math.round(lowColour.blue - (i * blueIncrement))
+    ));
   }
 
   return colourMap;
@@ -130,76 +129,66 @@ function createRandomColourRange(iterationRange, maxIterations) {
   let colourMap = new Map();
 
   //number of colours that we need to generate
-  let colourCount = iterationRange.higher-iterationRange.lower;
+  let colourCount = iterationRange.higher - iterationRange.lower;
 
-    for(let i=0;i<colourCount;i++) {
-      colourMap.set(iterationRange.lower+i,new Colour(Math.floor(Math.random()*255 +1),
-      Math.floor(Math.random()*255 +1),                                                
-      Math.floor(Math.random()*255 +1)
-                                                      ));
-    }
+  for (let i = 0; i < colourCount; i++) {
+    colourMap.set(iterationRange.lower + i, new Colour(Math.floor(Math.random() * 255 + 1),
+      Math.floor(Math.random() * 255 + 1),
+      Math.floor(Math.random() * 255 + 1)
+    ));
+  }
 
   //want to make sure that the set is always black
   if (iterationRange.higher == maxIterations) {
     colourMap.set(maxIterations, new Colour(0, 0, 0));
-  } 
-    return colourMap;
+  }
+  return colourMap;
 }
 
 function createBandWColourRange(iterationRange, maxIterations) {
   let colourMap = new Map();
 
   //number of colours that we need to generate
-  let colourCount = iterationRange.higher-iterationRange.lower;
-    let black = BLACK;
-    let white = new Colour(255,255,255);
+  let colourCount = iterationRange.higher - iterationRange.lower;
+  let black = BLACK;
+  let white = new Colour(255, 255, 255);
 
-    for(let i=0;i<colourCount;i++) {
-      let c = (i%2) ? white:black;
-      colourMap.set(iterationRange.lower+i,c);                                    
-    }
+  for (let i = 0; i < colourCount; i++) {
+    let c = (i % 2) ? white : black;
+    colourMap.set(iterationRange.lower + i, c);
+  }
 
   //want to make sure that the set is always black
   if (iterationRange.higher == maxIterations) {
     colourMap.set(maxIterations, black);
-  } 
+  }
   return colourMap;
 }
 
 
-  /* function ask(question, yes, no) {
-    if (confirm(question)) yes()
-    else no();
-  }
-  
-  ask(
-    "Do you agree?",
-    () =>  alert("You agreed.") ,
-    () =>  alert("You canceled the execution.")
-  ); */
 
-  /*
- * Convert hue-saturation-value/luminosity to RGB.
- *
- * Input ranges:
- *   H =   [0, 360] (integer degrees)
- *   S = [0.0, 1.0] (float)
- *   V = [0.0, 1.0] (float)
- */
-function hsv_to_rgb(h, s, v)
-{
-  if ( v > 1.0 ) v = 1.0;
-  var hp = h/60.0;
+/*
+* Convert HSV to RGB.
+* as per https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
+*
+* Input ranges:
+*   H =   [0, 360] (integer degrees)
+*   S = [0.0, 1.0] (float)
+*   V = [0.0, 1.0] (float)
+*/
+function hsv_to_rgb(h, s, v) {
+  if (v > 1.0) v = 1.0;
+  var hp = h / 60.0;
   var c = v * s;
-  var x = c*(1 - Math.abs((hp % 2) - 1));
-  var rgb = [0,0,0]; 
+  var x = c * (1 - Math.abs((hp % 2) - 1));
+  var rgb = undefined;
 
-  if ( 0<=hp && hp<=1 ) rgb = [c, x, 0];
-  if ( 1<hp && hp<=2 ) rgb = [x, c, 0];
-  if ( 2<hp && hp<=3 ) rgb = [0, c, x];
-  if ( 3<hp && hp<=4 ) rgb = [0, x, c];
-  if ( 4<hp && hp<=5 ) rgb = [x, 0, c];
-  if ( 5<hp && hp<=6 ) rgb = [c, 0, x];
+  if (0 <= hp && hp <= 1) rgb = [c, x, 0];
+  if (1 < hp && hp <= 2) rgb = [x, c, 0];
+  if (2 < hp && hp <= 3) rgb = [0, c, x];
+  if (3 < hp && hp <= 4) rgb = [0, x, c];
+  if (4 < hp && hp <= 5) rgb = [x, 0, c];
+  if (5 < hp && hp <= 6) rgb = [c, 0, x];
 
   var m = v - c;
   rgb[0] += m;
@@ -209,11 +198,11 @@ function hsv_to_rgb(h, s, v)
   rgb[0] *= 255;
   rgb[1] *= 255;
   rgb[2] *= 255;
-  return new Colour(rgb[0],rgb[1],rgb[2]);
+  return new Colour(rgb[0], rgb[1], rgb[2]);
 }
 
 
-let highColour= "#000000";
+let highColour = "#000000";
 let lowColour = "#ff0000";
 
 function handleHighColourChange(event) {
@@ -224,7 +213,36 @@ function handleLowColourChange(event) {
   lowColour = event.target.value;
 }
 
+// request mousemove events
+document.getElementById("myCanvas").onmousemove = function (e) { handleMouseMove(e); };
+document.getElementById("myCanvas").onmouseout = function (e) { handleMouseOut(e); };
 
+// show tooltip 
+function handleMouseMove(e) {
+  var canvas = document.getElementById('myCanvas');
+  var tipCanvas = document.getElementById("tip");
+  var tipCtx = tipCanvas.getContext("2d");
+  if (e.offsetX < 0 || e.offsetY < 0) return;
+  var point = getPointForPosition(e.offsetX,e.offsetY,canvas.width,canvas.height);
+  tipCanvas.style.left = (e.offsetX + 20) + "px";
+  tipCanvas.style.top = e.offsetY + "px";
+  tipCtx.clearRect(0, 0, tipCanvas.width, tipCanvas.height);
+  tipCtx.fillText(point.x+ "," + point.y + "  " + point.iteration + "," + point.smoothedIteration, 5, 15);
+}
+
+function handleMouseOut(e) {
+  var tipCanvas = document.getElementById("tip");
+  var tipCtx = tipCanvas.getContext("2d");
+  tipCanvas.style.left = "-200px";
+}
+
+function getPointForPosition(x,y,width,height) {
+  return pointArray[x + (y * width)];
+}
+
+
+//global variables
+var pointArray=undefined;
 
 function paint() {
 
@@ -239,10 +257,10 @@ function paint() {
   let ymax = Number(document.getElementById('ymax').value);
   let maxIterations = Number(document.getElementById('iterations').value);
   let escape = Number(document.getElementById('escape').value);
-  
-  let pointArray = initPoints(xmin,xmax,ymin,ymax,canvas.width, canvas.height);
+
+  pointArray = initPoints(xmin, xmax, ymin, ymax, canvas.width, canvas.height);
   // calculate the max iteration for each point, and the range
-  let iterationRange=calculate(pointArray,maxIterations,mbCalc,escape);
+  let iterationRange = calculate(pointArray, maxIterations, mbCalc, escape);
 
   // create a colour range
   let lowCol = document.getElementById('lowColour').value;
@@ -254,14 +272,14 @@ function paint() {
   //let colourMap = createRandomColourRange(iterationRange, maxIterations);
   //let colourMap = createBandWColourRange(iterationRange, maxIterations);
 
-  for (let i = 0,j=0; i < myImageData.data.length; i += 4,j++) {
+  for (let i = 0, j = 0; i < myImageData.data.length; i += 4, j++) {
     let p = pointArray[j];
 
-    let colour = (maxIterations==p.iteration) ? BLACK: hsv_to_rgb(360.0*p.smoothedIteration/maxIterations, 1.0, 1.0);// 10.0*p.smoothedIteration/iterations);
-      myImageData.data[i]     = colour.red; // red
-      myImageData.data[i + 1] = colour.green;   // green
-      myImageData.data[i + 2] = colour.blue; // blue
-      myImageData.data[i + 3] = colour.alpha; // alpha
+    let colour = (maxIterations == p.iteration) ? BLACK : hsv_to_rgb(360.0 * p.smoothedIteration / maxIterations, 1.0, 1.0);// 10.0*p.smoothedIteration/iterations);
+    myImageData.data[i] = colour.red; // red
+    myImageData.data[i + 1] = colour.green;   // green
+    myImageData.data[i + 2] = colour.blue; // blue
+    myImageData.data[i + 3] = colour.alpha; // alpha
   }
 
   ctx.putImageData(myImageData, 0, 0);
